1e50838973dc40bd087f523c5d924a063202ea6f
Solea Miller <soleamiller@gmail.com>
2025-11-03 10:34:45 -0500

    finished up to question 7, need more time

--- from-scratch.js
+++ from-scratch.js
@@ -10,18 +10,24 @@ const sampleWords = ['dog', 'blueberry', 'alabama', 'earth', 'football'];
 
 // Problem 1
 const printEveryWord = (words) => {
-
+ return words.forEach((word) => console.log(word));
 };
+
+printEveryWord(sampleWords);
 
 // Problem 2
 const getStringLengths = (strings) => {
-
+  return strings.map((string) => string.length)
 };
+
+console.log(getStringLengths(sampleWords));
 
 // Problem 3
 const sortWordsZtoA = (words) => {
-
+  return words.sort().reverse();
 };
+
+console.log(sortWordsZtoA(sampleWords));
 
 /* -------------------------------------------- */
 /* --------------NUMBER PROBLEMS--------------- */
@@ -31,13 +37,17 @@ const sampleNumbers = [7, 3, 9, 1, 4, 8, 2, 10, 5, 6];
 
 // Problem 4
 const getEvenNumbers = (nums) => {
-
+  return nums.filter((num) => num % 2 === 0);
 };
+
+console.log(getEvenNumbers(sampleNumbers));
 
 // Problem 5
 const getLargestNumber = (numbers) => {
-
+  return numbers.reduce((acc, num) => acc > num ? acc : num);
 };
+
+console.log(getLargestNumber(sampleNumbers));
 
 /* -------------------------------------------------- */
 /* --------------OBJECT ARRAY PROBLEMS--------------- */
@@ -53,15 +63,17 @@ const sampleUsers = [
 
 // Problem 6
 const printUserNamesAndScores = (users) => {
-
+  return users.map((user) => console.log(`${user.name} scored ${user.score} points.`));
 };
 
+printUserNamesAndScores(sampleUsers);
 
 // Problem 7
 const getUserNames = (users) => {
-
+  return users.map((user) => user.name);
 };
 
+console.log(getUserNames(sampleUsers));
 
 // Problem 8
 const getActiveUsersUnder30 = (users) => {
